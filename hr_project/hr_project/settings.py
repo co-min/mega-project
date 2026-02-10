df11b53d1d90d9f89089a10f4247fbb2d9906a97
정회윤 <uni030822@gmail.com>
2026-02-10 23:42:16 +0900

    데이터 베이스를 postgre sql로 수정함

--- hr_project/hr_project/settings.py
+++ hr_project/hr_project/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'hr_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mega_project_db',
+        'USER': 'admin',
+        'PASSWORD': 'mega1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
