5ff3b9a05468f483f3580e39da364f81042182cc
min <niobium41next42@duksung.ac.kr>
2026-02-27 07:11:58 +0900

    [update] requirements 추가

--- hr_project/hr_project/settings.py
+++ hr_project/hr_project/settings.py
@@ -11,16 +11,21 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
+env = environ.Env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+environ.Env.read_env(BASE_DIR / '.env')
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%=@81+&sr)r81=$+k2d#z=$zp)nuzd3&jjl0=s(%)@ae8pht6)'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
