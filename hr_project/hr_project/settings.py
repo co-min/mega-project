86ee1052e40c03d77092506e24119df6486a7c31
min <niobium41next42@duksung.ac.kr>
2026-03-28 20:36:09 +0900

    [update] settings.py for deploy

--- hr_project/hr_project/settings.py
+++ hr_project/hr_project/settings.py
@@ -32,7 +32,7 @@ environ.Env.read_env(BASE_DIR / '.env')
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "211.188.50.11",
